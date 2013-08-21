606917820c4429c4e118a79ba317af2e316678ad
Cam Incoll <incoll@gmail.com>
2013-08-21 20:26:09 +1000

    Created the Authentication System

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 InkBook::Application.routes.draw do
+  devise_for :users
+
   resources :statuses
 
   resources :clients
@@ -53,6 +55,8 @@ InkBook::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+
+  root :to => 'clients#index'
 
   # See how all your routes lay out with "rake routes"
 
