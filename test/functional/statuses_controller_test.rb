fac139088619763ef5505cf023fc9b6aa317d61e
Cam Incoll <incoll@gmail.com>
2013-08-26 17:31:12 +1000

    can only post, view, and update when logged in

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -17,15 +17,22 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
+  test "should be logged in to post status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should render the new page wehn logged in" do
     sign_in users(:jason)
     get :new
     assert_response :success
   end
 
-  test "should create status" do
+  test "should create status when logged in" do
+    sign_in users(:jason)
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content }
+        post :create, status: { content: @status.content }
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -36,12 +43,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get edit will be redirected when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should get edit" do
+    sign_in users(:jason)
     get :edit, id: @status
     assert_response :success
   end
 
+  test "should update status should redirect when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should update status" do
+    sign_in users(:jason)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
